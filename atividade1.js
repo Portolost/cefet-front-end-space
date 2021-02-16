3e5cddcc1dceeef740f48124aeb0b71c87ab62b3
Gabriel Comonian Porto <74382419+Portolost@users.noreply.github.com>
2021-02-15 22:56:32 -0300

    gabriel-comonian-cefet-front-end-space

--- atividade1.js
+++ atividade1.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+function expandirRetrair(e) {
+    let bot = e.currentTarget;
+    let divEl = bot.parentNode;
+    let click = divEl.classList.toggle('expandido');
+
+    if (click === true)
+        bot.innerHTML = "-";
+    else
+        bot.innerHTML = "+";
+}
+
+document.querySelectorAll('.botao-expandir-retrair').forEach(botEl => {
+    botEl.addEventListener('click', expandirRetrair);
+});
--- atividade2.js
+++ atividade2.js
@@ -15,5 +15,24 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
 // o índice da imagem sendo mostrada
 // (inicialmente, é a imagem 0: 'philae-parts.jpg')
 let indiceDaFotoCorrente = 0;
+let botAnt = document.querySelector('#anterior');
+let botPro = document.querySelector('#proximo');
+let imagem = document.querySelector('#slide');
+
+botAnt.addEventListener('click', function() {
+    indiceDaFotoCorrente--;
+    if (indiceDaFotoCorrente < 0)
+        indiceDaFotoCorrente = 4;
+
+    imagem.src = servidorDasImagens + nomesDasImagens[indiceDaFotoCorrente];
+});
+
+botPro.addEventListener('click', function() {
+  indiceDaFotoCorrente++;
+  if (indiceDaFotoCorrente > 4)
+      indiceDaFotoCorrente = 0;
+
+  imagem.src = servidorDasImagens + nomesDasImagens[indiceDaFotoCorrente];
+});
 
 // ...
